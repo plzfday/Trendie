57a0af3545d83d82d18d43a9a02321e1e8808e35
Dongyeon Park <plzfday@gmail.com>
2022-10-30 21:12:23 +0000

    fix: Edit the max length of the ticker

--- chart/models.py
+++ chart/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Ticker(models.Model):
-    ticker = models.CharField(max_length=6)
+    ticker = models.CharField(max_length=8)
     keywords = models.CharField(max_length=40)
 
     def __str__(self):

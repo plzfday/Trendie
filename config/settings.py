62e78debd346365a39b0e946e9676e9d04dd5d0e
Dongyeon Park <plzfday@gmail.com>
2022-11-17 16:23:04 +0000

    update: add new domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-f3*y&$w383_3^ec(28q0(6x@nm^$lv=f%bk2a&6-72+o_&qrjs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "trendie.site"]
 
 # Application definition
 

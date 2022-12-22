12cda73a7839f022f025fd554d9c8b8890c7dea6
plzfday <plzfday@gmail.com>
2022-12-22 14:27:12 +0000

    Change Ticker model into Company & Keyword models
    Need scalability to store information of a company.

--- chart/models.py
+++ chart/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 
-class Ticker(models.Model):
+class Company(models.Model):
     ticker = models.CharField(max_length=9)
-    keyword = models.CharField(max_length=40)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.ticker
+
+
+class Keyword(models.Model):
+    id = models.AutoField(primary_key=True)
+    keyword = models.CharField(max_length=40)
+    ticker_id = models.ForeignKey(Company, on_delete=models.CASCADE)
